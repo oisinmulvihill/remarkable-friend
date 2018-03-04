28e96e133111df503f0b1e682215548404e2a4ed
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2018-03-04 18:32:40 +0000

    experimenting with struct to unpack

--- rmfriend/lines/format.py
+++ rmfriend/lines/format.py
@@ -14,6 +14,8 @@ Using inspiration from
 
 """
 import io
+import struct
+
 
 binary_format = [
     {"name": "header", "bytes": 43},
@@ -24,17 +26,26 @@ binary_format = [
 def read_int32_t(stream):
     return int.from_bytes(stream.read(4), byteorder='little')
 
+def read_int32_t(stream):
+    return int.from_bytes(stream.read(4), byteorder='little')
+
 
 def parse(lines):
     """
     """
     fd = io.BytesIO(lines)
+    #header = fd.read(43).decode('ascii')
 
-    header = fd.read(43).decode('ascii')
+    expected = b'reMarkable lines with selections and layers'
+    header, pages = struct.unpack_from('<{}sI'.format(len(expected)), lines)
 
-    pages = read_int32_t(fd)
+    # pages = struct.iter_unpack('<I', fd)
 
-    print("header: {}\npages: {}".format(header, pages))
+    print("\nheader: {}\npages: {}".format(header, pages))
+
+    #pages = read_int32_t(fd)
+
+    return
 
     for page in range(pages):
         print("Page Number: {}".format(page))
@@ -50,7 +61,7 @@ def parse(lines):
                 brush_type = read_int32_t(fd)
                 colour = read_int32_t(fd)
                 unknown = read_int32_t(fd)
-                unknown = read_int32_t(fd)
+                brush_size = read_float(fd)
                 print("line Number: {}".format(line))
 
 
